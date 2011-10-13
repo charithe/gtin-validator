249d9a2eb65abbe91f8833da61410cfd67b155b3
Charith Ellawala <charith@lucidelectricdreams.com>
2011-10-14 00:03:21 +0100

    Initial commit

---
+++ gtin_validator.py
@@ -0,0 +1,68 @@
+# GTIN validation routines
+# Copyright 2011 Charith Ellawala (charith@lucidelectricdreams.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+""" Validates GTIN (Global Trade Item Number) strings by calculating checksums.
+
+Supports GTIN-8,GTIN-12,GTIN-13 and GTIN-14. Checksum calculation is done using
+the method defined at http://www.gs1.org/barcodes/support/check_digit_calculator.
+
+"""
+
+def is_valid_GTIN(code):
+  """ Validates any GTIN-8, GTIN-12, GTIN-13 or GTIN-14 code. """
+  cleaned_code = _clean(code)
+
+  return _is_valid_code(cleaned_code)
+
+
+def _clean(code):
+  if isinstance(code,(int,long)):
+	return str(code).zfill(14)
+  elif isinstance(code,basestring):
+	return code.replace("-","").strip().zfill(14)
+  else:
+	raise TypeError("Expected string or integer type as input parameter")
+
+
+def _is_valid_code(code):
+  code_length = len(code)
+
+  if not code.isdigit():
+	return False
+  elif not ((code_length == 8) or (code_length == 12) or (code_length == 13) or (code_length == 14) or (code_length == 18)):
+	return False
+  else:
+	return _is_gtin_checksum_valid(code)
+
+
+def _is_gtin_checksum_valid(code):
+  code_length = len(code)
+  total = 0
+
+  for i in xrange(1,code_length):
+	if i % 2 == 0:
+	  total = total + int(code[i-1])
+	else:
+	  total = total + (3 * int(code[i-1]))
+
+  check_digit = 10 - (total % 10)
+  return int(code[code_length - 1]) == check_digit
+
+
+
+
+
+
+
---
+++ gtin_validator_test.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+import unittest
+from gtin_validator import *
+
+class GTINValidatorTest(unittest.TestCase):
+
+  def test_incorrect_length_string(self):
+	self.assertFalse(is_valid_GTIN("1234567"))
+
+  def test_incorrect_length_number(self):
+	self.assertFalse(is_valid_GTIN(12345678910))
+
+  def test_correct_length_string_with_spaces(self):
+	self.assertFalse(is_valid_GTIN(" 1234567891 "))
+
+  def test_correct_length_string_with_dashes(self):
+	self.assertFalse(is_valid_GTIN("123-456-7890"))
+
+  def test_alphanumeric_string(self):
+	self.assertFalse(is_valid_GTIN("98795147A"))
+
+
+  def test_valid_gtin8_string_no_leading_zeros(self):
+	self.assertTrue(is_valid_GTIN("98795147"))
+
+  def test_valid_gtin8_string_with_leading_zeros(self):
+	self.assertTrue(is_valid_GTIN("0000098795147"))
+
+  def test_valid_gtin8_string_with_leading_zeros_and_dashes(self):
+	self.assertTrue(is_valid_GTIN("000-0-098-79514-7"))
+
+  def test_valid_gtin8_number(self):
+	self.assertTrue(is_valid_GTIN(98795147))
+
+  def test_gtin8_invalid_check_digit(self):
+	self.assertFalse(is_valid_GTIN("98795145"))
+
+  
+  def test_valid_gtin12_string_no_leading_zeros(self):
+	self.assertTrue(is_valid_GTIN("951753852654"))
+
+  def test_valid_gtin12_string_with_leading_zeros(self):
+	self.assertTrue(is_valid_GTIN("0951753852654"))
+
+  def test_valid_gtin12_string_with_leading_zeros_and_dashes(self):
+	self.assertTrue(is_valid_GTIN("095-1-753-85265-4"))
+
+  def test_valid_gtin12_number(self):
+	self.assertTrue(is_valid_GTIN(951753852654))
+
+  def test_gtin12_invalid_check_digit(self):
+	self.assertFalse(is_valid_GTIN("951753852651"))
+  
+  
+  def test_valid_gtin13_string(self):
+	self.assertTrue(is_valid_GTIN("9780552133265"))
+
+  def test_valid_gtin13_string_with_dashes(self):
+	self.assertTrue(is_valid_GTIN("978-0-552-13326-5"))
+
+  def test_valid_gtin13_number(self):
+	self.assertTrue(is_valid_GTIN(9780552133265))
+
+  def test_gtin13_invalid_check_digit(self):
+	self.assertFalse(is_valid_GTIN("9780552133262"))
+
+
+  def test_valid_gtin14_string(self):
+	self.assertTrue(is_valid_GTIN("95135725845679"))
+
+  def test_valid_gtin14_number(self):
+	self.assertTrue(is_valid_GTIN(95135725845679))
+
+  def test_gtin14_invalid_check_digit(self):
+	self.assertFalse(is_valid_GTIN("95135725845672"))
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  unittest.main()
+
